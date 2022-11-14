12972be79cd1f7c19ed9097fb0a14e067d17e37d
beast462 <binhmattroi@gmail.com>
2022-11-14 08:43:06 +0700

    add component config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,9 +6,17 @@ export default defineConfig({
   videosFolder: './dist/videos',
   downloadsFolder: './dist/downloads',
   screenshotsFolder: './dist/screenshots',
+
   e2e: {
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
 });
